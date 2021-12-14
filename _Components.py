61ee9b5d92217678481bcbb95c6ea49caa9d4c66
LapdogMikki <lapdog.mikki@gmail.com>
2021-12-15 00:06:11 +0400

    Добавлена функция размещения кнопок

--- _Components.py
+++ _Components.py
@@ -1,9 +1,9 @@
 import tkinter as tkntr
-from tkinter.constants import HORIZONTAL
+from tkinter.constants import ANCHOR, HORIZONTAL, SW
 import tkinter.ttk as tkttk
 
 class MFormTabFrame(tkntr.Frame):
-    def __init__(self,parent,headings=tuple(), rows=tuple()):
+    def __init__(self,parent,headings=tuple(), rows=tuple(),btns=list()):
         tkntr.Frame.__init__(self,parent,background="white")
         self.parent = parent
         table = tkttk.Treeview(self, show="headings", selectmode="browse")
@@ -21,6 +21,17 @@ class MFormTabFrame(tkntr.Frame):
         scrolltable.pack(side=tkntr.RIGHT, fill=tkntr.Y)
         xscrolltable.pack(side=tkntr.BOTTOM, fill=tkntr.X)
         table.pack(expand=tkntr.YES, fill=tkntr.BOTH)
-        self.initUI()  
+        
+        self.initUI() 
+    def create_btns(self,g):
+        btns=[]
+        frbt=tkntr.Frame()
+        frbt.pack(side=tkntr.BOTTOM)
+        for i in range(0,g):
+            btns.append(tkntr.Button(frbt,text='5'))    
+        for btn in btns:
+            btn.pack(side=tkntr.LEFT,anchor=SW) 
+                   
     def initUI(self):
         self.pack(fill=tkntr.BOTH,expand=1) 
+        
