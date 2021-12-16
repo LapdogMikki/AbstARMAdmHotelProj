89ad1e83f4338f1b4a74ca1d215e2bb27599f9d3
LapdogMikki <lapdog.mikki@gmail.com>
2021-12-16 11:40:36 +0400

    изменения в оболочке

--- _Components.py
+++ _Components.py
@@ -3,7 +3,7 @@ from tkinter.constants import HORIZONTAL
 import tkinter.ttk as tkttk
 
 class MFormTabFrame(tkntr.Frame):
-    def __init__(self,parent,headings=tuple(), rows=tuple()):
+    def __init__(self,parent,headings=tuple(), rows=tuple(),hb=int(),he=int(),btns=list(),lbls=list(),empts=list()):
         tkntr.Frame.__init__(self,parent,background="white")
         self.parent = parent
         tablecomps=tkntr.Frame(self,background="white")
@@ -23,19 +23,46 @@ class MFormTabFrame(tkntr.Frame):
         xscrolltable.pack(side=tkntr.BOTTOM, fill=tkntr.X)
         table.pack(expand=tkntr.YES, fill=tkntr.BOTH)
         tablecomps.pack(side=tkntr.TOP,fill=tkntr.X)
-        datecomps=tkntr.Frame(self,background='black')
-        a=tkntr.Label(datecomps,text="АААА")
-        self.btns=list()
-        a.pack(side=tkntr.RIGHT)
-        datecomps.pack(side=tkntr.BOTTOM,fill=tkntr.BOTH)
+        datecomps=tkntr.Frame(self,background="white")
+        btnframe=tkntr.Frame(datecomps,background="white")
+        lblframe=tkntr.Frame(datecomps,background="white")
+        emptsframe=tkntr.Frame(datecomps,background="white")
+        btns=create_btns(btnframe,hb)
+        self.btns=btns
+        lbls=create_lbls(lblframe,headings)
+        self.lbls=lbls
+        empts=create_txts(emptsframe,he)
+        self.empts=empts
+        btnframe.pack(anchor=tkntr.N,fill=tkntr.BOTH)
+        lblframe.pack(side=tkntr.LEFT,anchor=tkntr.SE,fill=tkntr.BOTH)
+        emptsframe.pack(side=tkntr.RIGHT,anchor=tkntr.SW,fill=tkntr.BOTH)
+        datecomps.pack(fill=tkntr.BOTH)
         self.initUI() 
-    
-    def create_btns(self,g):
-        listhdbtns=['Добавить','Сохранить','Удалить','Отмена','Поиск','Обновление']
-        for i in range(0,g):
-            self.btns.append(tkntr.Button(text=listhdbtns[i])) 
-        for btn in self.btns:
-            btn.pack(side=tkntr.BOTTOM,anchor=tkntr.S)  
+            
                    
     def initUI(self):
         self.pack(fill=tkntr.BOTH,expand=1) 
+        
+def create_btns(frame,g):
+    btns=[]
+    listhdbtns=['Добавить','Сохранить','Удалить','Отмена','Поиск','Обновление']
+    for i in range(0,g):
+        btns.append(tkntr.Button(frame,text=listhdbtns[i])) 
+    for btn in btns:
+        btn.pack(side=tkntr.LEFT,anchor=tkntr.S,padx=100,ipadx=20,pady=5)  
+    return btns
+def create_lbls(frame,nheadings):
+    lbls=[]
+    listhdlbls=list(nheadings)
+    for i in range(0,len(listhdlbls)):
+        lbls.append(tkntr.Label(frame,text=listhdlbls[i],background="white")) 
+    for lbl in lbls:
+        lbl.pack(side=tkntr.TOP,anchor=tkntr.S,padx=75,ipadx=30,pady=3) 
+    return lbls 
+def create_txts(frame,g):
+    entrs=[]
+    for i in range(0,g):
+        entrs.append(tkntr.Entry(frame)) 
+    for entr in entrs:
+        entr.pack(side=tkntr.TOP,anchor=tkntr.S,padx=180,ipadx=150,pady=4) 
+    return entrs 
